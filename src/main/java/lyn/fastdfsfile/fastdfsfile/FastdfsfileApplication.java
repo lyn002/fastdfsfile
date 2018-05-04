d1afbd6d7b46fa4c67d03b6f14323de7a3aa75c6
lyn002 <865857170@qq.com>
2018-05-04 15:14:55 +0800

    在@SpringBootApplication中排除其注入 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class,HibernateJpaAutoConfiguration.class})
    //@SpringBootApplication

--- src/main/java/lyn/fastdfsfile/fastdfsfile/FastdfsfileApplication.java
+++ src/main/java/lyn/fastdfsfile/fastdfsfile/FastdfsfileApplication.java
@@ -4,14 +4,17 @@ import com.sun.org.apache.bcel.internal.util.ClassPath;
 import com.sun.org.apache.xpath.internal.SourceTree;
 import lyn.fastdfsfile.fastdfsfile.service.fastdfsFileService;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class,HibernateJpaAutoConfiguration.class})
 public class FastdfsfileApplication {
 
 	public static void main(String[] args) throws IOException {
